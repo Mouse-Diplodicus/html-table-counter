835e2cfc7d595572de913f43d603b6a5efee04ba
Mouse-Diplodicus <williamm1.matt@yahoo.com>
2024-03-21 20:35:30 -0600

    initial version

--- table-count.py
+++ table-count.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+import logging
 
 
 def main() -> int:
@@ -11,7 +12,41 @@ def main() -> int:
     args = parser.parse_args()
 
     filename = args.filename
+    if not filename:
+        logging.error('Missing filename, quitting')
+        return 1
+
+    count(filename)
+
     return 0
+
+
+def count(filename) -> None:
+    tbl_count = 0
+    first_col = False
+    rows, columns = 0, 0
+    srcfile = open(filename)
+    results = open('results.csv', 'w')
+    results.write('Table_Title, Table_num, Num_columns, Num_Rows,\n')
+    for line in srcfile:
+        if '<table' in line:
+            tbl_count += 1
+            first_col = True
+        if '<tr' in line:
+            columns += 1
+        if first_col and '<td' in line:
+            rows += 1
+        if first_col and '</tr>' in line:
+            first_col = False
+        if '<!--' in line:
+            results.write(line.split("\n")[0])
+        if '</table>' in line:
+            results.write(f",{tbl_count}, {columns}, {rows},\n")
+            rows = 0
+            columns = 0
+
+    srcfile.close()
+    results.close()
 
 
 if __name__ == '__main__':
---
+++ tests/test_table-count.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestTableCount(unittest.TestCase):
+    def test_main(self):
+        self.assertEqual(True, False)
+
+    def test_count(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
